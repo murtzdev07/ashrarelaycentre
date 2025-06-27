0304b3d2a0539043e442ac335639453cb6c0dff9
Azizkharwa18 <azizkharwawala@gmail.com>
2025-06-27 08:22:20 +0530

    pushing

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -4,7 +4,137 @@ import { FaBullhorn } from 'react-icons/fa';
 
 const Announcements = () => {
   const announcements = [
-      {
+    {
+    title: 'Ashara Mubaraka 1447h, (Relay center-Ratlam)',
+    message: (
+      <div>
+        <p>
+          Afzalus Salam
+
+Ashara Mubaraka Ratlam Relay Centre ma Raza si Aaunar Sagla Mehmano no Ahlan Wasahlan 
+Dohad na bilad si aaaunaar mumeneen je Delhi-Mumbai Expressway si thata huwa Ganthela Bordi taraf left lai ane Ratlam Sagod Road  taraf Right Lai  te baad Varoth Mata Mandir si straight Aawe  aap ne Parking Zone ma direct karwa waaste BGI na khidmatguzaro yaari aapse aap sagla co-operate krso em adaban Iltemas che 
+
+        </p>
+      
+        <p><strong>Parking Location : 
+</strong></p>
+        <p>https://maps.app.goo.gl/UQFjGTc6tx3KCwWU6</p>
+      
+        <p><strong>Shukran</strong></p>
+      </div>
+    ),
+    date: '26th June 2025',
+  },
+    {
+    title: 'Ashara Mubaraka 1447h, (Relay center-Ratlam)',
+    message: (
+      <div>
+        <p>
+          Waaz Mubarak:
+        </p>
+        <ul>
+          <li> All Zone Gates will open @8:30am. </li>
+        </ul>
+         <p>
+         Raat Majlis: 
+        </p>
+        <ul>
+          <li> All zone Gates will open @5:30pm </li>
+        </ul>
+         <p>
+           KALIMI DOME & MASJID gate information: 
+        </p>
+        <ul>
+          <li> G-1 - For gents, Kalimi Masjid </li>
+          <li>G-2 - For gents, DOME </li>
+          <li> G-3 - For gents, Rahat Dome </li>
+          <li> L-1 - For Ladies, Kalimi Masjid </li>
+          <li> L-2 - For Ladies, Dome </li>
+          <li> L-3 - For Ladies, Rahat Dome  </li>
+        </ul>
+        <p>
+          Mumeenen waqat jem mention kidu che eem hazir thai. 
+        </p>
+        <p><strong>Shukran</strong></p>
+      </div>
+    ),
+    date: '26th June 2025',
+  },
+    {
+    title: 'Ashara Mubaraka 1447h, (Relay center-Ratlam)',
+    message: (
+      <div>
+        <p>
+          Afternoon & evening programs during Ashara Mubaraka:
+        </p>
+        <ul>
+          <li>Mukhayyam ul Quran: (for all mumeneen & farzando)  All age
+            Venue: Taheri Mohallah tehfeez & Kalimi Dome
+            Time: 3:30pm to 5:00pm (Daily)</li>
+          <li>Waaz ISTIBSAR activity for BAIRAO (All age)
+            Venue: Taheri Mohallah tehfeez & Kalimi Dome
+            Time: 5:00pm to 5:30pm (Daily)
+          </li>
+          <li>Farzando ISTIBSAR (5 to 15 Age)
+            Venue: ALL WAAZ VENUES
+            Time: 5:30pm to 5:55pm</li>
+          <li>General ISTIBSAR for ALL MUMENEEN (mardo & bairao)
+            Venue: ALL WAAZ VENUES
+            Time: 5:55pm to 6:25pm</li>
+          <li>Youngsters ISTIBSAR session (16-25 Age)
+            Venue: SAIFY masjid Sehen & BURHANI masjid Sehen
+            Time: 9:30 to 10:00pm </li>
+          <li>Medicines jem zarurat hoi emm carry kare</li>
+          <li>Je mumineen HOTELS ma hoi ye taharat na asbab ni tayyari kari ne aawe (chakhri, paglu, bichonu)</li>
+          <li>Waaz vaste shoe bag carry kare</li>
+          <li>For more support and details, mehmano can click the following links:</li>
+        </ul>
+        <p>
+          Sagla Mumeneen aa programs ma shamil thai ane Ashara Mubaraka ni har ghari ni khair ma istemal kare.
+        </p>
+        <p><strong>Shukran</strong></p>
+      </div>
+    ),
+    date: '26th June 2025',
+  },
+  {
+    title: 'Important Checklist for Mumeneen Mehmano',
+    message: (
+      <div>
+        <p>
+          Mumeneen Mehmano je Ashara Mubaraka waaste Ratlam aawnar che ye sagla waste checklist:
+        </p>
+        <ul>
+          <li>Safar shuru kare toh safar ni dua ni tilawat kare</li>
+          <li>Nazrul Maqam ane Sadaqo kare</li>
+          <li>Ratlam pohnchi ne arrival scanning karawe (Venue: NTSS school & Railway station)</li>
+          <li>Rainy season che - filhal ghano barish che (zaruri cheezo sathe carry kare)</li>
+          <li>Kapra ni tayyari barabar si kari ne aawe - laundry easily available nathi</li>
+          <li>Medicines jem zarurat hoi emm carry kare</li>
+          <li>Je mumineen HOTELS ma hoi ye taharat na asbab ni tayyari kari ne aawe (chakhri, paglu, bichonu)</li>
+          <li>Waaz vaste shoe bag carry kare</li>
+          <li>For more support and details, mehmano can click the following links:</li>
+        </ul>
+        <p>
+          Sagla Mehmaano na ITS par RAZA verify thaaya pachhi HOF na ghare utaaro allocate karwa ma aavse.
+        </p>
+        <ul>
+          <li>18mi Tarikh pehla lag aa details Accomodation Office ma aavi ne verify karavi le</li>
+          <li>Mehmaan na Ratlam Allocation no ITS no Screenshot bhi saathe laave</li>
+          <li>Accomodation na Khidmatguzaro Fajere 09:00 si 10:00 ane raate 9:00 si 11:00 waaje Saifee Jamaat Office ma haazir rehse</li>
+        </ul>
+        <p><strong>Zone-wise Contacts:</strong></p>
+        <ul>
+          <li><strong>Helpline number :-</strong>+919243206802</li>
+          <li><strong>Helpline Email :-</strong> helplinedeskratlam@gmail.com</li>
+          <li><strong>Website :- </strong><a href="https://www.ratlamrelaycentre.co.in" target="_blank" rel="noopener noreferrer">https://www.ratlamrelaycentre.co.in</a></li>
+          <li><strong>Helpline form :-</strong><a href="https://www.ratlamrelaycentre.co.in/helpline" target="_blank" rel="noopener noreferrer">https://www.ratlamrelaycentre.co.in/helpline</a></li>
+        </ul>
+      </div>
+    ),
+    date: '23rd June 2025',
+  },
+  {
     title: 'Accomodation Verification',
     message: (
       <div>
@@ -35,24 +165,24 @@ const Announcements = () => {
     ),
     date: '11th June 2025',
   },
-    {
-      title: 'Relay Center Allocation',
-      message: (
-        <p>
-          Relay Center selection will start at 14mi Zilhijjatil Haram - 10th June 2025, 11 AM (IST) on{' '}
-          <a href="https://miqaat.its52.com" target="_blank" rel="noopener noreferrer">
-            miqaat.its52.com
-          </a>.
-        </p>
-      ),
-      date: '10th June 2025',
-    },
-    {
-      title: 'Raza Notice ',
-      message: 'Mumineen must make travel arrangements only after receiving Raza for Ashara Mubarakah 1447H at Ratlam Relay Centre.',
-      date: '9th June 2025',
-    },
-    
+  {
+    title: 'Relay Center Allocation',
+    message: (
+      <p>
+        Relay Center selection will start at 14mi Zilhijjatil Haram - 10th June 2025, 11 AM (IST) on{' '}
+        <a href="https://miqaat.its52.com" target="_blank" rel="noopener noreferrer">
+          miqaat.its52.com
+        </a>.
+      </p>
+    ),
+    date: '10th June 2025',
+  },
+  {
+    title: 'Raza Notice ',
+    message: 'Mumineen must make travel arrangements only after receiving Raza for Ashara Mubarakah 1447H at Ratlam Relay Centre.',
+    date: '9th June 2025',
+  },
+
   ];
 
   return (
@@ -70,7 +200,7 @@ const Announcements = () => {
           ))}
         </div>
       </div>
-       </div> 
+    </div>
   );
 };
 
